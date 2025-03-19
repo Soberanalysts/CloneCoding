4b68e672b533b0e27434935c7125ce8adb9b9870
soberanalysts <nss262@naver.com>
2025-03-19 20:20:02 +0900

    'ApexLineChart'

---
+++ Frontend/Apexchart/linechart.js
@@ -0,0 +1,16 @@
+var options = {
+    chart: {
+        type: 'line',
+        height: 350
+    },
+    series: [{
+        name: '판매량',
+        data: [10, 40, 35, 50, 49, 60, 70, 91, 125]
+    }],
+    xaxis: {
+        categories: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월']
+    }
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
